8ca8a2c8896ccc95224e7561222e698bd3ff9a8a
Dan Olver <devdolphin7@outlook.com>
2024-08-01 12:31:29 +0100

    Set up routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Browse from "../routes/Browse.jsx";
+import AddItem from "../routes/SellAddItem.jsx";
+import ViewItems from "../routes/SellViewItems.jsx";
+import Basket from "../routes/Basket.jsx";
+import OrderHistory from "../routes/AccountOrderHistory.jsx";
+import Kudos from "../routes/AccountKudos.jsx";
+import Create from "../routes/AccountCreate.jsx";
+
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Browse />} />
+      <Route path="/Sell/AddItem" element={<AddItem />} />
+      <Route path="/Sell/ViewItems" element={<ViewItems />} />
+      <Route path="/Basket" element={<Basket />} />
+      <Route path="/Account/OrderHistory" element={<OrderHistory />} />
+      <Route path="/Account/Kudos" element={<Kudos />} />
+      <Route path="/Account/Create" element={<Create />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
