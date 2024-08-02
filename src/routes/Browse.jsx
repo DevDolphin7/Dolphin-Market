0e2ff78a7914976bd23b12c905ca6f5498cb0b27
Dan Olver <devdolphin7@outlook.com>
2024-08-02 17:28:24 +0100

    Add Browse route

--- src/routes/Browse.jsx
+++ src/routes/Browse.jsx
@@ -1,6 +1,30 @@
-import MainContent from '../components/MainContent'
+import { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Section from "../components/Section/Section";
+import { getItems } from "../utils/api";
+
 export default function Browse() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [category, setCategory] = useState("");
+  const [browseItems, setBrowseItems] = useState([]);
+
+  // Fetch data to browse
+  useEffect(() => {
+    setIsLoading(true);
+    getItems(category === "" ? undefined : category)
+      .then((items) => {
+        setBrowseItems(items);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [category]);
+
   return (
-   <MainContent/>
+    <>
+      <Header setCategory={setCategory} />
+      {isLoading ? <h1>Loading...</h1> : <Section browseItems={browseItems} />}
+    </>
   );
 }
