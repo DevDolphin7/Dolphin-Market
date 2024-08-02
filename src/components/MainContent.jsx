d529b05d16b5f55033753e4a2811ac832e6617b2
Dan Olver <devdolphin7@outlook.com>
2024-08-02 17:27:02 +0100

    Remove MainContent - replaced by react router - individual route files

--- src/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-import Header from './Header.jsx'
-import {useState} from 'react'
-
-
-export default function MainContent () {
-    const [category, setCategory] = useState('')
-    console.log(category)
-    return <Header setCategory={setCategory}/>
-}
