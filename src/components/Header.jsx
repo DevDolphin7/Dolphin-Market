ac28c6f5cd58039259a94e9786a503b2577b5264
Dan Olver <devdolphin7@outlook.com>
2024-08-02 17:26:36 +0100

    Create and style Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import DropdownButton from './DropdownButton.jsx'
-import './Header.css'
-export default function Header ({setCategory}) {
-    const dropdownInfo = {label: 'Category', buttons: ['Electronics', 'Relics', 'Household'], defaultText: 'All', setState: setCategory }
-    return (
-        <DropdownButton className='header' dropdownInfo={dropdownInfo}/>
+import DropdownButton from "./DropdownButton.jsx";
+import "./Header.css";
 
-    
-    
-    )
+export default function Header({ setCategory }) {
+  const dropdownInfo = {
+    label: "Category",
+    buttons: ["Electronics", "Clothing", "Household"],
+    defaultText: "All",
+    setState: setCategory,
+  };
+
+  return (
+    <div className="header">
+      <DropdownButton dropdownInfo={dropdownInfo} />
+    </div>
+  );
 }
